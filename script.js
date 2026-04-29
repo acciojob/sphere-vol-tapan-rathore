89f6fd2d17333a37e3cc7d12f33ee9d20271b7f3
Tapan Rathore <tapanrathore07@gmail.com>
2026-04-30 00:18:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
+	e.preventDefault()
+	
+	let r = parseFloat(document.getElementById("radius").value);
+	
+	let volume = (4/3) * Math.PI * Math.pow(r,3); 
+	console.log(r, volume)
+	
+	document.getElementById("volume").value = volume.toFixed(2)
   
 } 
 
